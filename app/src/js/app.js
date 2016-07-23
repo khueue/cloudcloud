91f26bd6990266a9d8d7b255bf20b3ea17ad18df
Sebastian Lundström <sebastian.lundstrom@viaplay.com>
2016-07-23 14:54:55 +0200

    Basic Cycle.js implementation

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,53 +1,63 @@
 import {run} from '@cycle/xstream-run';
-import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+import xs from 'xstream';
+import {makeDOMDriver} from '@cycle/dom';
+import {div, textarea, label, br, hr} from '@cycle/dom';
+import CfnReducer from 'cfn-reducer';
 
 function main(sources) {
+	function reduce(templateText) {
+		let reduced = templateText;
+		try {
+			const template = JSON.parse(templateText);
+			const cfnReducer = new CfnReducer(template, {
+				stackParams: {
+					hello: 'replaced',
+				},
+			});
+			reduced = cfnReducer.reduce();
+			reduced = JSON.stringify(reduced, null, '  ');
+		} catch (e) {}
+		return reduced;
+	}
+
+	const startTemplate = JSON.stringify({
+		'Fn::Equals': [
+			'value',
+			'value',
+		],
+	}, null, '  ');
+
+	const inputTemplate$ = sources.DOM
+		.select('#input-template')
+		.events('input')
+		.map(ev => ev.target.value)
+		.startWith(startTemplate);
+
+	const state$ = inputTemplate$.map(inputTemplate => {
+		return {
+			input: inputTemplate,
+			output: reduce(inputTemplate),
+		};
+	});
+
 	const sinks = {
-		DOM: sources.DOM.select('.field').events('input')
-			.map(ev => ev.target.value)
-			.startWith('')
-			.map(name =>
-				div([
-					label('Name:'),
-					input('.field', {attrs: {type: 'text'}}),
-					hr(),
-					h1('Hello ' + name),
-				])
-			)
+		DOM: state$.map(state => {
+			return div([
+				label('Input template'),
+				br(),
+				textarea('#input-template', {style: {width: '300px', height: '150px'}}, state.input),
+				hr(),
+				label('Output template'),
+				br(),
+				textarea('#output-template', {style: {width: '300px', height: '150px'}}, state.output),
+			]);
+		}),
 	};
 	return sinks;
 }
 
-run(main, {
+const drivers = {
 	DOM: makeDOMDriver('#app'),
-});
-
-/*
-'use strict';
-
-var CfnReducer = require('cfn-reducer');
-
-var template = {
-	hello: 'test',
 };
 
-// Specify necessary parameters.
-var stackParams = {
-	MyParam1: 'some-value-1',
-	MyParam2: 'some-value-2',
-};
-
-// Run the reducer.
-var options = {
-	stackParams: stackParams,
-};
-var reducer = new CfnReducer(template, options);
-var reduced = reducer.reduce();
-
-// Show me the magic!
-var output = JSON.stringify(reduced, null, '\t');
-
-setTimeout(function () {
-	document.body.innerHTML = output;
-}, 500);
-*/
+run(main, drivers);
